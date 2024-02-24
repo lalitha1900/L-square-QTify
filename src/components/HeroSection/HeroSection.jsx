91bb6ca843b341f0605ab7b2203b1fe9cc07c9e8
lalitha1900 <trlalitha2000@gmail.com>
2024-02-24 22:24:48 +0530

    push input

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import Styles from "./HeroSection.module.css";
-import { ReactComponent as VibratingHeadphone } from "../../assets/hero_headphones.png";
-
-const HeroSection = () => {
+import { ReactComponent as HeroImg } from "../../assets/hero_headphones.png";
+import styles from "./HeroSection.module.css";
+export default function HeroSection() {
   return (
-    <div className={Styles.HeroContainer}>
-      <div className={Styles.HeroText}>
-        <div>100 Thousand Songs, ad-free</div>
-        <div>Over thousands podcast episodes</div>
+    <div className={styles.herosection}>
+      <div>
+        <h1 className={styles.heroText}>100 Thousand Songs, ad-free</h1>
+        <h1 className={styles.heroText}>Over thousands podcast episodes</h1>
       </div>
-      <VibratingHeadphone />
+      <HeroImg className={styles.heroImg} />
     </div>
   );
-};
-
-export default HeroSection;
+}
