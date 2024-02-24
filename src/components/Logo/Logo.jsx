584eafe946c5ace7d9d9bbdbfa37dc23fa61464c
lalitha1900 <trlalitha2000@gmail.com>
2024-02-24 18:23:59 +0530

    push input

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,9 +3,9 @@ import LogoImage from "../../assets/logo.png"
 const Logo = () => {
 	const style = {'padding-left':'32px'};
   return (
-    <div>
+   
         <img style={style} src={LogoImage} alt='logo' width={67} />
-        </div>
+        
   )
 }
 
