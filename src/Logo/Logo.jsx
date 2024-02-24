40617bbf35dda90dd621cb80b8454c1473bf9f02
lalitha1900 <trlalitha2000@gmail.com>
2024-02-24 14:30:49 +0530

    push input

---
+++ src/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BrandLogo from "../../assets/logo.png"
+const Logo = () => {
+  return (
+    <div>
+        <img src={BrandLogo} alt='logo' width={67} />
+        </div>
+  )
+}
+
+export default Logo
