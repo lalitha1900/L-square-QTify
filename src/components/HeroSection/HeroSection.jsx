7558d8cde0a9cd1097c26b153384059e52902be0
lalitha1900 <trlalitha2000@gmail.com>
2024-02-24 23:31:36 +0530

    push input

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,45 +1,23 @@
-
-import React, { useEffect, useRef } from "react";
-import { ReactComponent as HeroImg } from "../../assets/hero_headphones.png";
+import React from "react";
 import styles from "./HeroSection.module.css";
+import Headphone from "../../assets/hero_headphones.png";
 
-export default function HeroSection() {
-  const parentRef = useRef(null);
+const HeroSection = () => {
+	return (
+		<>
+			<div className={styles.wrapper}>
+				<div className={styles.heroImageWrapper}>
+					<div className={styles.banner}>
+						<h1>100 Thousand Songs, ad-free</h1>
+						<h1>Over thousands podcast episodes</h1>
+					</div>
+					<div>
+						<img src={Headphone} alt="Headphone" height="212px" width="212px" />
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
-  useEffect(() => {
-    // Function to find the parent element with specified text and image
-    const findParentElement = () => {
-      const elements = document.querySelectorAll(`.${styles.herosection} h1.${styles.heroText} + ${styles.heroImg}`);
-      
-      for (const element of elements) {
-        const text = element.previousElementSibling.textContent.trim().toLowerCase();
-        if (text === "100 thousand songs, ad-free") {
-          return element.parentElement;
-        }
-      }
-
-      return null;
-    };
-
-    // Find the parent element
-    const parentElement = findParentElement();
-
-    if (parentElement) {
-      // Check the background color of the parent element
-      const backgroundColor = window.getComputedStyle(parentElement).getPropertyValue("background-color");
-      console.log("Background Color:", backgroundColor);
-    } else {
-      console.error("Parent element not found");
-    }
-  }, []);
-
-  return (
-    <div ref={parentRef} className={styles.herosection}>
-      <div>
-        <h1 className={styles.heroText}>100 Thousand Songs, ad-free</h1>
-        <h1 className={styles.heroText}>Over thousands podcast episodes</h1>
-      </div>
-      <HeroImg className={styles.heroImg} />
-    </div>
-  );
-}
+export default HeroSection;
