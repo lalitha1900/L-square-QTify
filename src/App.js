ad7ac302aa66c72d6f34f49b43e7f880c6e23bce
lalitha1900 <trlalitha2000@gmail.com>
2024-02-25 12:42:47 +0530

    push input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 import HeroSection from './components/HeroSection/HeroSection';
 
 function App() {
   return (
+    <>
+    <NavBar/>
    <HeroSection/>
+   </>
+   
   );
 }
 
